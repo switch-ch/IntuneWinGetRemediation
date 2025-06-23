df9c245cc120d1db2a1bbc5bada6bb8fa2a6dc47
David Sass <10754765+sassdawe@users.noreply.github.com>
2025-06-23 21:33:35 +0200

    feat(Winget) Configure Winget

---
+++ 0-DesktopAppInstaller/Check-DesktopAppInstaller.ps1
@@ -0,0 +1,48 @@
+﻿<# Winget updater
+    Created by David Sass
+    Created: 2024-06-20
+#>
+
+<# details
+  1. Check if it is installed, if not install it.
+  2. If installed check its version and compare it to the latest in Winget, if there is an update install it
+  3. When the latest is installed, do nothing.
+#>
+$logPath = "c:\ProgramData\IntuneRemediation\DesktopAppInstaller-Check.txt"
+new-item $logPath -ItemType File -Force | Out-null
+
+Start-Transcript -Path $logPath -Force
+
+$ResolveWingetPath = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe\winget.exe"
+if ($ResolveWingetPath) {
+    $WingetPath = $ResolveWingetPath[-1].Path
+    $Wingetpath = Split-Path -Path $WingetPath -Parent
+    Set-Location $wingetpath
+
+    if ( $null -eq (Get-AppxPackage Microsoft.DesktopAppInstaller) ) {
+        Write-Host "No Microsoft.DesktopAppInstaller package has been found"
+        Stop-Transcript
+        exit 1
+    }
+    else {
+        Write-Output "Microsoft.DesktopAppInstaller version check"
+
+        # get information of latest version
+        $available = .\winget.exe search --exact --id Microsoft.AppInstaller --source winget | Select-Object -Last 1 | ConvertFrom-Csv -Delimiter " " -Header "name", "Id", "Version"
+        
+        # get installed version
+        $installed = Get-AppxPackage Microsoft.DesktopAppInstaller
+    
+        # compare version numbers
+        if ( $installed.Version -eq $available.Version ) {
+            Write-Output "DesktopAppInstaller latest version installed"
+        }
+        else {
+            Write-Output "Need to install DesktopAppInstaller to device scope!"
+            
+            Stop-Transcript
+            exit 1
+        }
+    }
+}
+Stop-Transcript
---
+++ 0-DesktopAppInstaller/Update-DesktopAppInstaller.ps1
@@ -0,0 +1,42 @@
+﻿<# Winget updater
+    Created by David Sass
+    Created: 2024-06-20
+#>
+
+<# details
+    1. Check if it is installed, if not install it.
+    2. If installed check its version and compare it to the latest in Winget, if there is an update install it
+    3. When the latest is installed, do nothing.
+#>
+$logPath = "c:\ProgramData\IntuneRemediation\DesktopAppInstaller-Remedy.txt"
+New-Item $logPath -ItemType File -Force | Out-Null
+
+Start-Transcript -Path $logPath -Force
+
+$ResolveWingetPath = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe\winget.exe"
+if ($ResolveWingetPath) {
+    $WingetPath = $ResolveWingetPath[-1].Path
+    $Wingetpath = Split-Path -Path $WingetPath -Parent
+    Set-Location $wingetpath
+
+    Write-Output "Microsoft.DesktopAppInstaller Installer"
+
+    # get information of latest version
+    $available = .\winget.exe search --exact --id Microsoft.AppInstaller --source winget | Select-Object -Last 1 | ConvertFrom-Csv -Delimiter " " -Header "name", "Id", "Version"
+    
+    # get installed version
+    $installed = Get-AppxPackage Microsoft.DesktopAppInstaller -ea SilentlyContinue
+
+    # compare version numbers
+    if ( $(try { $installed.Version -eq $available.Version } catch { $false }) ) {
+        Write-Output "Mattermost Desktop latest version installed"
+    }
+    else {
+        Write-Output "Installing DesktopAppInstaller to device scope"
+
+        .\winget.exe upgrade --exact --id Microsoft.AppInstaller --silent --accept-package-agreements --accept-source-agreements --scope machine --force
+    }
+
+}
+
+Stop-Transcript
